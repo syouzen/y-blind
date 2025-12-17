dfa087f8ea276e404b52032e7aacf596f2f5027e
syouzen <seungyeon.choi1026@gmail.com>
2025-12-17 22:24:07 +0900

    feat: add token check and redirect in sign-in page

--- src/app/(home)/sign-in/page.tsx
+++ src/app/(home)/sign-in/page.tsx
@@ -1,5 +1,16 @@
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+
+import { getCookie } from "cookies-next/server";
+
 import SignInForm from "./_components/sign-in-form";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const token = await getCookie("access_token", { cookies });
+
+  if (token) {
+    redirect("/", RedirectType.replace);
+  }
+
   return <SignInForm />;
 }
--- src/app/(home)/sign-up/page.tsx
+++ src/app/(home)/sign-up/page.tsx
@@ -1,5 +1,16 @@
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+
+import { getCookie } from "cookies-next/server";
+
 import SignUpForm from "./_components/sign-up-form";
 
-export default function SignUp() {
+export default async function SignUp() {
+  const token = await getCookie("access_token", { cookies });
+
+  if (token) {
+    redirect("/", RedirectType.replace);
+  }
+
   return <SignUpForm />;
 }
